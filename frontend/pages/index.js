c78c8d55093d212acf7c430f9ac24447612ab19b
Supriya P <supriyaprakash2229@gmail.com>
2022-12-17 11:04:46 +0530

    changes to frontend

---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+
+import styles from '../styles/Home.module.css'
+
+import Image from 'next/image'
+export default function Home() {
+  return (
+    <>
+    
+     <div className=" bg-[url('../public/bg.png')] h-full w-full bg-cover bg-center ">
+     
+
+    </div>
+    
+    </>
+  )
+}
